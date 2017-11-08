45acbb8aff81b5fdde58918b38b85eed814bd2db
Jeremy Valance <jvalance@pcm-jvalance-1.local>
2017-11-08 18:18:00 -0500

    Added more files

---
+++ shop.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Dan Gluck Training</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+    <script src="https://use.fontawesome.com/5e8412d3f3.js"></script>
+
+  <body class="shop-page">
+    <div class="main-container">
+    <!-- Header top -->
+        <div class="header-container">
+            <div class="header-top">
+                <div class="header-top-inner row">
+                    <div class="header-top-left col">
+                        <a href="#" class="header-email">DanGluckTraining@gmail.com</a>
+                        <span class="header-phone">(802) 881-5190</span>
+                    </div>                
+                    <div class="header-top-right col">
+                        <div class="header-logos">
+                            <ul class="header-logos-list">
+                                <li class="logo__item"><a href="https://twitter.com/GluckTraining"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                                <li class="logo__item"><a href="https://www.facebook.com/DanGluckTraining/"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                                <li class="logo__item"><a href="https://www.instagram.com/danglucktraining/"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    <!-- Header bottom -->
+
+            <div class="header-bottom">
+                <div class="header-bottom-inner row">
+                    <div class="header-bottom-left col">
+                        <div class="logo-wrap">
+                            <a href="/">
+                                <img src="images/DanGluck_logo.png" class="logo"/>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="header-bottom-right col">
+                        <nav class="menu">
+                            <ul class="top-menu">
+                                <li class="top-menu-item"><a class="top-menu-link" href="/gluck-website/home.php">Home</a></li>
+                                <li class="top-menu-item"><a class="top-menu-link" href="/gluck-website/shop.php">Shop</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div> 
+
+    <!-- Main Content --> 
+
+    <section class="main-content">
+        <div class="main-content-inner container">
+            <div class="row shop-row">
+            <div class="shop-item">
+                <a class="shop-image-link" href="#">
+                        <div class="shop-item-image-wrap">
+                            <img src="images/weights-3.png" class="shop-image"/>
+                        </div>
+                        <h4 class="shop-item-title">Online Personal Training (1 Program)</h4>
+                        <p class="shop-item-price">$40.00</p>
+                </a>
+                </div>
+                <div class="shop-item">
+                <a class="shop-image-link" href="#">
+                        <div class="shop-item-image-wrap">
+                            <img src="images/weights-1.png" class="shop-image"/>
+                        </div>
+                        <h4 class="shop-item-title">Online Person Training (2 Programs)</h4>
+                        <p class="shop-item-price">$70.00</p>
+                </a>
+                </div>
+                <div class="shop-item">
+                <a class="shop-image-link" href="#">
+                        <div class="shop-item-image-wrap">
+                            <img src="images/weights-2.png" class="shop-image"/>
+                        </div>
+                        <h4 class="shop-item-title">Online Personal Training (3 Programs)</h4>
+                        <p class="shop-item-price">$95.00</p>
+                </a>
+                </div>
+            </div>
+            <div class="row shop-row">
+            <div class="shop-item">
+                <a class="shop-image-link" href="#">
+                        <div class="shop-item-image-wrap">
+                            <img src="images/weights-3.png" class="shop-image"/>
+                        </div>
+                        <h4 class="shop-item-title">Online Coaching</h4>
+                        <p class="shop-item-price">$35.00</p>
+                </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Footer -->
+
+    <footer class="footer">
+        <div class="footer-inner container">
+            <div class="footer-inner-contact">
+                <p class="footer-email">DanGluckTraining@gmail.com</p>
+                <p class="footer-phone">(802) 881-5190</p>
+                <p class="footer-city">South Burlington, VT, USA</p>
+            </div>
+            <div class="footer-logos row">
+                <ul class="footer-logos-list">
+                    <li class="logo__item"><a href="https://twitter.com/GluckTraining"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                    <li class="logo__item"><a href="https://www.facebook.com/DanGluckTraining/"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                    <li class="logo__item"><a href="https://www.instagram.com/danglucktraining/"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                 </ul>
+            </div>
+        </div>
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+  </body>
+</html>
